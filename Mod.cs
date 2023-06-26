88905d61dd5f969b67e37f7d268148e362c517aa
adon <86583577+shg166@users.noreply.github.com>
2023-06-27 08:44:47 +0900

    Add Code & Name List

---
+++ Mod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+using ColossalFramework.UI;
+using ICities;
+
+namespace JP_IndustryNameList
+{
+	public class JP_IndustryNameList : IUserMod
+	{
+
+		public string Name
+		{
+			get { return "JP Industry Name List (CNL) v1.0"; }
+		}
+
+		public string Description
+		{
+			get { return "Custom Name Listsを使って、DLC Industriesの産業エリア名を変更します"; }
+		}
+
+	}
+}
